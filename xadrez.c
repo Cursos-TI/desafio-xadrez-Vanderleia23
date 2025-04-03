11632c2890bfdea5f1ce3dd973b0ff052faa64b9
Vanderleia Cristina Mingotti <cristinamingotti3@outlook.com>
2025-04-03 14:01:07 +0000

    finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
     
     int torre = 1;
     int bispo = 1;
     int rainha = 1;
     int cavalo = 1;
+
+    printf("###Peças do jogo de Xadrez### \n");
 
     //Movimento da Torre
 
